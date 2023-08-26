bb598191805656e0ef7427ad1b349110e9bf2ffb
Sergio <sergioruza22@gmail.com>
2023-08-26 09:16:43 -0300

    feat: logic to print data

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,26 +1,38 @@
 import java.util.Scanner;
 
 public class ContaTerminal {
-  int numero;
+  public int numero;
   String agencia;
   String nomeCliente;
   float saldo;
+
   public static void main(String[] args) {
     final Scanner sc1 = new Scanner(System.in);
     ContaTerminal conta = new ContaTerminal();
 
     System.out.print("Digite o número da conta: ");
     conta.numero = sc1.nextInt();
-
+    
     System.out.print("Digite sua agência: ");
     conta.agencia = sc1.next();
+    sc1.nextLine();
 
     System.out.print("Digite o nome do cliente: ");
-    conta.nomeCliente = sc1.next();
+    conta.nomeCliente = sc1.nextLine();
 
     System.out.print("Digite o saldo: ");
     conta.saldo = sc1.nextFloat();
 
     sc1.close();
+
+    conta.imprimirDados();
+  }
+  
+  public void imprimirDados() {
+    System.out.println("/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-");
+    System.out.println("Número da conta: " + numero);
+    System.out.println("Número da agẽncia: " + agencia);
+    System.out.println("Nome do cliente: " + nomeCliente);
+    System.out.println("Saldo em conta: " + saldo);
   }
 }
