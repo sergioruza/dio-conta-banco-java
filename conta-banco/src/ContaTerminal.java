bd5f0769d207cde5adb76d83a422f6a682fa296e
Sergio <sergioruza22@gmail.com>
2023-08-26 08:55:13 -0300

    feat: logic for data input

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
+  int numero;
+  String agencia;
+  String nomeCliente;
+  float saldo;
   public static void main(String[] args) {
-    
+    final Scanner sc1 = new Scanner(System.in);
+    ContaTerminal conta = new ContaTerminal();
+
+    System.out.print("Digite o número da conta: ");
+    conta.numero = sc1.nextInt();
+
+    System.out.print("Digite sua agência: ");
+    conta.agencia = sc1.next();
+
+    System.out.print("Digite o nome do cliente: ");
+    conta.nomeCliente = sc1.next();
+
+    System.out.print("Digite o saldo: ");
+    conta.saldo = sc1.nextFloat();
+
+    sc1.close();
   }
 }
